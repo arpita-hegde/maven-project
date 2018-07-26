fcad500eb297f76a4c2be728749c2989bc9315b8
arpita-hegde <arpitachegde@gmail.com>
2018-07-26 12:45:52 +0530

    maven-project

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * !
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "maven project!" );
+    }
+}
